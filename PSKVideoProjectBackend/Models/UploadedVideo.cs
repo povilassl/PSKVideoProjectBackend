8bb73490af523d424b016684c9eef10cb81304c9
povilassl <slivinskas.povilas@gmail.com>
2023-03-25 20:43:52 +0200

    adding db + default controllers

---
+++ PSKVideoProjectBackend/Models/UploadedVideo.cs
@@ -0,0 +1,33 @@
+﻿using PSKVideoProjectBackend.Properties;
+using System.ComponentModel.DataAnnotations;
+
+namespace PSKVideoProjectBackend.Models
+{
+    public class UploadedVideo
+    {
+        [Key]
+        public long Id { get; set; }
+        public String Name { get; set; }
+        public String URL { get; set; }
+        public uint VideoLengthInSeconds { get; set; }
+        public DateTime UploadDateTime { get; set; }
+        public uint LikeCount { get; set; }
+        public uint DislikeCount { get; set; }
+        public uint ViewCount { get; set; }
+        public String Username { get; set; }
+        public String Description { get; set; }
+
+        public UploadedVideo()
+        {
+            Name = Resources.FillerVideoName;
+            URL = Resources.FillerVideoURL;
+            VideoLengthInSeconds = 0;
+            UploadDateTime = DateTime.Now;
+            LikeCount = 0;
+            DislikeCount = 0;
+            ViewCount = 0;
+            Username = Resources.FillerVideoUsername;
+            Description = Resources.FillerVideoDescription;
+        }
+    }
+}
---
+++ PSKVideoProjectBackend/Repositories/VideoRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PSKVideoProjectBackend.Models;
+
+namespace PSKVideoProjectBackend.Repositories
+{
+    public class VideoRepository
+    {
+        private readonly ApiDbContext _apiDbContext;
+
+        public VideoRepository(ApiDbContext apiDbContext)
+        {
+            _apiDbContext = apiDbContext;
+        }
+
+        public async Task<IEnumerable<UploadedVideo>>? GetListOfVideos(int startIndex, int count)
+        {
+            var allVideos = await _apiDbContext.UploadedVideos.ToListAsync();
+
+            if (startIndex + count > allVideos.Count) return null;
+
+            return allVideos.GetRange(startIndex, count).ToList();
+        }
+
+        public async Task<UploadedVideo> UploadVideo(UploadedVideo video)
+        {
+            video.LikeCount = 0;
+            video.DislikeCount = 0;
+            video.ViewCount = 0;
+
+            var result = await _apiDbContext.UploadedVideos.AddAsync(video);
+            await _apiDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+    }
+}
