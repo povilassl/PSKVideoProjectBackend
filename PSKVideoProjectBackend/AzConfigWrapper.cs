3d77ea0be03477602eca128dba523b6649bfc31d
povilassl <slivinskas.povilas@gmail.com>
2023-04-04 21:06:03 +0300

    start working on upload

---
+++ PSKVideoProjectBackend/AzConfigWrapper.cs
@@ -0,0 +1,57 @@
+﻿namespace DemoVideoApplication
+{
+    public class AzConfigWrapper
+    {
+        private readonly IConfiguration _config;
+
+        public AzConfigWrapper(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string SubscriptionId
+        {
+            get { return _config["AZURE_SUBSCRIPTION_ID"]!; }
+        }
+
+        public string ResourceGroup
+        {
+            get { return _config["AZURE_RESOURCE_GROUP"]!; }
+        }
+
+        public string AccountName
+        {
+            get { return _config["AZURE_MEDIA_SERVICES_ACCOUNT_NAME"]!; }
+        }
+
+        public string AadTenantId
+        {
+            get { return _config["AZURE_TENANT_ID"]!; }
+        }
+
+        public string AadClientId
+        {
+            get { return _config["AZURE_CLIENT_ID"]!; }
+        }
+
+        public string AadSecret
+        {
+            get { return _config["AZURE_CLIENT_SECRET"]!; }
+        }
+
+        public Uri ArmAadAudience
+        {
+            get { return new Uri(_config["AZURE_ARM_TOKEN_AUDIENCE"]!); }
+        }
+
+        public Uri AadEndpoint
+        {
+            get { return new Uri(_config["AadEndpoint"]!); }
+        }
+
+        public Uri ArmEndpoint
+        {
+            get { return new Uri(_config["AZURE_ARM_ENDPOINT"]!); }
+        }
+    }
+}
--- PSKVideoProjectBackend/AzureMediaManager.cs
+++ PSKVideoProjectBackend/AzureMediaManager.cs
@@ -1,7 +1,49 @@
-﻿namespace PSKVideoProjectBackend
-{
-    public class AzureMediaManager
-    {
+﻿using DemoVideoApplication;
+using Azure.ResourceManager.Media;
+using Azure.ResourceManager.Media.Models;
 
+namespace PSKVideoProjectBackend
+{
+    public static class AzureMediaManager
+    {
+        public static IAzureMediaServicesClient _client = null;
+        public static AzConfigWrapper? _config = null;
+
+        public static async Task InitManager()
+        {
+            Console.WriteLine("Dir:" + Directory.GetCurrentDirectory());
+
+            AzConfigWrapper config = new AzConfigWrapper(new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("configappsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build());
+
+            try
+            {
+                IAzureMediaServicesClient client = await CreateMediaServicesClientAsync(config);
+
+                Console.WriteLine("connected");
+
+                _client = client;
+                _config = config;
+            }
+            catch (Exception exception)
+            {
+                if (exception.Source.Contains("ActiveDirectory"))
+                {
+                    Console.Error.WriteLine("TIP: Make sure that you have filled out the appsettings.json file before running this sample.");
+                }
+
+                Console.Error.WriteLine($"{exception.Message}");
+
+
+                if (exception.GetBaseException() is ErrorResponseException apiException)
+                {
+                    Console.Error.WriteLine(
+                        $"ERROR: API call failed with error code '{apiException.Body.Error.Code}' and message '{apiException.Body.Error.Message}'.");
+                }
+            }
+        }
     }
 }
