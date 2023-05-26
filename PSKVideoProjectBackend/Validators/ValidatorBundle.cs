24822eef23f7f91ef5026529a542f419f164c1ed
povilassl <slivinskas.povilas@gmail.com>
2023-05-26 21:50:04 +0300

    some progress on new validation implementation

---
+++ PSKVideoProjectBackend/Validators/ValidatorBundle.cs
@@ -0,0 +1,32 @@
+﻿using PSKVideoProjectBackend.Interfaces;
+
+namespace PSKVideoProjectBackend.Validators
+{
+    public class ValidatorBundle
+    {
+        public class UsernameValidator : IValidator
+        {
+            public bool IsValid(string username)
+            {
+                return true;
+            }
+        }
+
+        public class PasswordValidator : IValidator
+        {
+            public bool IsValid(string password)
+            {
+                return true;
+            }
+        }
+
+        public class EmailValidator : IValidator
+        {
+            public bool IsValid(string email)
+            {
+                return true;
+            }
+        }
+    }
+
+}
